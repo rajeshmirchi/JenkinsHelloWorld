2b2c626470e008c8bab197bc9fb3c2286caf2b4e
rajeshmirchi <30932740+rajeshmirchi@users.noreply.github.com>
2023-05-22 00:33:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 public static void main(String args[])
 {
-	System.out.println("Welcome to HelloWorld!!!!");
+	System.out.println("Welcome to HelloWorld by changes");
 }
 
 }
